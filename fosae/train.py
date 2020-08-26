d7637e3436bdd67815843aa67945d9353105e62f
Chengxi Yang <yangchen@oregonstate.edu>
2020-08-25 18:53:59 -0700

    add contrastive loss

--- fosae/train.py
+++ fosae/train.py
@@ -16,7 +16,7 @@ TRAIN_BZ = 200
 TEST_BZ = 190
 ALPHA = 100
 BETA = 100
-MARGIN = 4
+MARGIN = 4.0
 
 
 print("Model is FOSAE")
@@ -38,7 +38,7 @@ def action_loss_function(pred, preds_next, action, criterion=nn.MSELoss(reductio
 def contrastive_loss_function(pred, preds_next, criterion=nn.MSELoss(reduction='none')):
     sum_dim = [i for i in range(1, pred.dim())]
     MSE = criterion(pred, preds_next).sum(dim=sum_dim).mean()
-    return torch.max(torch.tensor(0), torch.tensor(MARGIN) - MSE) * BETA
+    return torch.max(torch.tensor(0.0), torch.tensor(MARGIN) - MSE)[0] * BETA
 
 
 def train(dataloader, vae, temp, optimizer):
