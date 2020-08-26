28c4a8d4593a36b3350d68acb6e1e9d2308c60ea
Chengxi Yang <yangchen@oregonstate.edu>
2020-08-26 05:15:17 -0700

    add contrastive loss

--- fosae/train.py
+++ fosae/train.py
@@ -149,7 +149,7 @@ def run(n_epoch):
     vae = eval(MODEL_NAME)()
     # load_model(vae)
     optimizer = Adam(vae.parameters(), lr=1e-3)
-    scheculer = LambdaLR(optimizer, lambda e: 1.0 if e < 10 else 0.1)
+    scheculer = LambdaLR(optimizer, lambda e: 1.0 if e < 100 else 0.1)
     best_loss = float('inf')
     for e in range(n_epoch):
         temp = np.maximum(TEMP_BEGIN * np.exp(-ANNEAL_RATE * e), TEMP_MIN)
