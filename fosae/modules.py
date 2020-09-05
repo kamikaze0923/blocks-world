8e8ab41d9ac36d4940ba03aac5deab9286db45e7
Chengxi Yang <yangchen@oregonstate.edu>
2020-09-05 14:04:54 -0700

    add contrastive loss

--- fosae/modules.py
+++ fosae/modules.py
@@ -28,15 +28,15 @@ class BaseObjectImageEncoder(nn.Module):
         super(BaseObjectImageEncoder, self).__init__()
         self.in_objects = in_objects
         self.conv1 = nn.Conv2d(in_channels=in_objects*IMG_C, out_channels=CONV_CHANNELS, kernel_size=(8,8), stride=(4,4), padding=2)
-        self.bn1 = nn.BatchNorm2d(CONV_CHANNELS)
+        # self.bn1 = nn.BatchNorm2d(CONV_CHANNELS)
         self.fc2 = nn.Linear(in_features=CONV_CHANNELS*FMAP_H*FMAP_W, out_features=ENCODER_FC_LAYER_SIZE)
-        self.bn2 = nn.BatchNorm1d(1)
+        # self.bn2 = nn.BatchNorm1d(1)
         self.fc3 = nn.Linear(in_features=ENCODER_FC_LAYER_SIZE, out_features=out_features)
 
     def forward(self, input):
-        h1 = self.bn1(torch.relu(self.conv1(input.view(-1, self.in_objects * IMG_C, IMG_H, IMG_W))))
+        h1 = torch.relu(self.conv1(input.view(-1, self.in_objects * IMG_C, IMG_H, IMG_W)))
         h1 = h1.view(-1, 1, CONV_CHANNELS * FMAP_H * FMAP_W)
-        h2 = self.bn2(torch.relu(self.fc2(h1)))
+        h2 = torch.relu(self.fc2(h1))
         return self.fc3(h2)
 
 class PredicateNetwork(nn.Module):
@@ -105,11 +105,11 @@ class PredicateDecoder(nn.Module):
     def __init__(self):
         super(PredicateDecoder, self).__init__()
         self.fc1 = nn.Linear(in_features=P*N**A, out_features=DECODER_FC_LAYER_SIZE)
-        self.bn1 = nn.BatchNorm1d(1)
+        # self.bn1 = nn.BatchNorm1d(1)
         self.fc2 = nn.Linear(in_features=DECODER_FC_LAYER_SIZE, out_features=N*IMG_C*IMG_H*IMG_W)
 
     def forward(self, input):
-        h1 = self.bn1(torch.relu(self.fc1(input.view(-1, 1, P*N**A))))
+        h1 = torch.relu(self.fc1(input.view(-1, 1, P*N**A)))
         return torch.sigmoid(self.fc2(h1)).view(-1, N, IMG_C, IMG_H, IMG_W)
 
 class FoSae(nn.Module):
