bb3e4ae92b955cc0f1162ba7ba4954b680fa3f36
Chengxi Yang <yangchen@oregonstate.edu>
2020-09-21 09:57:56 -0700

    training simple blocks world

--- c_swm/utils.py
+++ c_swm/utils.py
@@ -226,7 +226,7 @@ class StateTransitionsDataset(data.Dataset):
         rand_other_state_suc = self.sample_diff_key(tuple(self.experience_buffer[ep]['scene_state_suc']))
 
         return obs, next_obs, obj_mask, next_obj_mask, action_mov_obj_index, action_tar_obj_index, \
-               self.all_scene_dict[rand_other_state_pre][0], self.all_scene_dict[rand_other_state_suc][0], \
+               self.all_scene_dict[rand_other_state_pre][0][step], self.all_scene_dict[rand_other_state_suc][0][step], \
                self.all_scene_dict[rand_other_state_pre][1], self.all_scene_dict[rand_other_state_suc][1],
 
     def sample_diff_key(self, key):
