98834175cf0d23b8dc9cd0a66f1cb4306ef51680
Chengxi Yang <yangchen@oregonstate.edu>
2020-09-23 12:23:54 -0700

    training simple blocks world

--- fosae/get_view_data.py
+++ fosae/get_view_data.py
@@ -1,10 +1,10 @@
-from c_swm.utils import StateTransitionsDataset
+from c_swm.utils import StateTransitionsDataset, Concat
 from fosae.modules import FoSae, FoSae_Action
 from fosae.gumble import device
 import numpy as np
 from torch.utils.data import DataLoader
 import torch
-from fosae.modules import IMG_H, IMG_W, IMG_C, A, N, U, P
+from fosae.modules import IMG_H, IMG_W, IMG_C, A, P
 from fosae.modules import OBJS, STACKS, REMOVE_BG
 from fosae.train_fosae import PREFIX
 from fosae.train_fosae import MODEL_NAME as FOSAE_MODEL_NAME
@@ -12,13 +12,18 @@ from fosae.train_action_model import ACTION_MODEL_NAME
 
 N_OBJ = OBJS + STACKS + (0 if REMOVE_BG else 1)
 N_EXAMPLES = 96
+MAX_N = 7
 
 
 def init():
-    test_set = StateTransitionsDataset(hdf5_file="c_swm/data/{}_all.h5".format(PREFIX), n_obj=OBJS+STACKS, remove_bg=REMOVE_BG)
-    print("View examples {}".format(len(test_set)))
+    train_set = Concat(
+        [StateTransitionsDataset(
+            hdf5_file="c_swm/data/blocks-{}-{}-det_all.h5".format(OBJS, STACKS), n_obj=OBJS + STACKS, remove_bg=False, max_n_obj=MAX_N
+        ) for OBJS in [1,2]]
+    )
+    print("Training Examples: {}".format(len(train_set)))
 
-    view_loader = DataLoader(test_set, batch_size=N_EXAMPLES, shuffle=True)
+    view_loader = DataLoader(train_set, batch_size=N_EXAMPLES, shuffle=True)
 
     vae = FoSae().to(device)
     vae.load_state_dict(torch.load("fosae/model_{}/{}.pth".format(PREFIX, FOSAE_MODEL_NAME), map_location='cpu'))
@@ -36,38 +41,37 @@ def run(vae, action_model, view_loader):
 
         data = view_loader.__iter__().__next__()
 
-        _, _, _, obj_mask, next_obj_mask, action_mov_obj_index, action_tar_obj_index = data
+        _, _, obj_mask, next_obj_mask, action_mov_obj_index, action_from_obj_index, action_tar_obj_index, n_obj, \
+        _, _, obj_mask_tilda, _ = data
+
         data = obj_mask.to(device)
         data_next = next_obj_mask.to(device)
+        data_tilda = obj_mask_tilda.to(device)
 
-        recon_batch, preds = vae((data, data_next), 0)
+        preds = vae((data, data_next, data_tilda, n_obj), 0)
 
-        data_np = data.view(-1, N, IMG_C, IMG_H, IMG_W).detach().cpu().numpy()
-        recon_batch_np = recon_batch[0].view(-1, N, IMG_C, IMG_H, IMG_W).detach().cpu().numpy()
-        preds_np = preds[0].detach().cpu().numpy().reshape(-1, P, N, N)
+        data_np = data.view(-1, MAX_N, IMG_C, IMG_H, IMG_W).detach().cpu().numpy()
+        preds_np = preds[0].detach().cpu().numpy().reshape(-1, P, MAX_N, MAX_N)
 
-        print(data_np.shape, recon_batch_np.shape, preds_np.shape)
+        print(data_np.shape, preds_np.shape)
         np.save("fosae/block_data/block_data.npy", data_np)
-        np.save("fosae/block_data/block_rec.npy", recon_batch_np)
         np.save("fosae/block_data/block_preds.npy", preds_np)
 
-        data_np = data_next.view(-1, N, IMG_C, IMG_H, IMG_W).detach().cpu().numpy()
-        recon_batch_np = recon_batch[1].view(-1, N, IMG_C, IMG_H, IMG_W).detach().cpu().numpy()
-        preds_np = preds[1].detach().cpu().numpy().reshape(-1, P, N, N)
+        data_np = data_next.view(-1, MAX_N, IMG_C, IMG_H, IMG_W).detach().cpu().numpy()
+        preds_np = preds[1].detach().cpu().numpy().reshape(-1, P, MAX_N, MAX_N)
 
-        print(data_np.shape, recon_batch_np.shape, preds_np.shape)
+        print(data_np.shape, preds_np.shape)
         np.save("fosae/block_data/block_data_next.npy", data_np)
-        np.save("fosae/block_data/block_rec_next.npy", recon_batch_np)
         np.save("fosae/block_data/block_preds_next.npy", preds_np)
 
 
-        action_idx = torch.cat([action_mov_obj_index, action_tar_obj_index], dim=1).to(device)
+        action_idx = torch.cat([action_mov_obj_index, action_from_obj_index, action_tar_obj_index], dim=1).to(device)
         batch_idx = torch.arange(action_idx.size()[0])
-        batch_idx = torch.stack([batch_idx, batch_idx], dim=1).to(device)
+        batch_idx = torch.stack([batch_idx for _ in range(action_idx.size()[1])], dim=1).to(device)
         action = obj_mask[batch_idx, action_idx, : , :, :].to(device)
 
-        actions = action_model((data, action), 0)
-        action_np = actions.detach().cpu().numpy().reshape(-1, P, N, N)
+        actions = action_model((data, action, n_obj), 0)
+        action_np = actions.detach().cpu().numpy().reshape(-1, P, MAX_N, MAX_N)
         print(action_np.shape)
         np.save("fosae/block_data/action.npy", action_np)
 
--- fosae/view_plot.py
+++ fosae/view_plot.py
@@ -1,28 +1,22 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import pickle
-from fosae.modules import IMG_H, IMG_W, IMG_C, A, N, U, P
-
+from fosae.get_view_data import MAX_N
 
 def dis(preds, preds_next):
     for a,b in zip(preds, preds_next):
         print(a-b)
         print("-"*10)
 
-
-
 data = np.load("fosae/block_data/block_data.npy")
-rec_batch = np.load("fosae/block_data/block_rec.npy")
 preds = np.load("fosae/block_data/block_preds.npy")
 
 data_next = np.load("fosae/block_data/block_data_next.npy")
-rec_batch_next = np.load("fosae/block_data/block_rec_next.npy")
 preds_next = np.load("fosae/block_data/block_preds_next.npy")
 
 action = np.load("fosae/block_data/action.npy")
 
 
-fig, axs = plt.subplots(7, N, figsize=(8, 6))
+fig, axs = plt.subplots(5, MAX_N, figsize=(8, 6))
 for _, ax in np.ndenumerate(axs):
     ax.set_xticks([])
     ax.set_yticks([])
@@ -33,20 +27,17 @@ def show_img(ax, arr):
 
 
 while True:
-    for one_data, one_rec_batch, one_data_next, one_rec_batch_next, one_p, one_p_nt, one_a in zip(
-        data, rec_batch, data_next, rec_batch_next, preds, preds_next, action
+    for one_data, one_data_next, one_p, one_p_nt, one_a in zip(
+        data, data_next, preds, preds_next, action
     ):
-        for i, (d, r, d_nt, r_nt) in enumerate(zip(one_data, one_rec_batch, one_data_next, one_rec_batch_next)):
-            show_img(axs[0,i], r)
-            show_img(axs[1,i], r_nt)
-            show_img(axs[2,i], d)
-            show_img(axs[3,i], d_nt)
-
+        for i, (d, d_nt) in enumerate(zip(one_data, one_data_next)):
+            show_img(axs[0,i], d)
+            show_img(axs[1,i], d_nt)
 
         for i, (p, p_nt, a) in enumerate(zip(one_p, one_p_nt, one_a)):
-            axs[4,i].imshow(p, cmap='gray')
-            axs[5,i].imshow(p_nt, cmap='gray')
-            axs[6,i].imshow(a, cmap='gray')
+            axs[2,i].imshow(p, cmap='gray')
+            axs[3,i].imshow(p_nt, cmap='gray')
+            axs[4,i].imshow(a, cmap='gray')
 
         plt.pause(0.2)
         # a = 1
