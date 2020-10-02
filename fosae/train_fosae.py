6d431aa2b2829e3d87502d91d66c057b1c95bc26
Chengxi Yang <yangchen@oregonstate.edu>
2020-10-02 12:57:47 -0700

    before start new model

--- fosae/train_fosae.py
+++ fosae/train_fosae.py
@@ -52,7 +52,12 @@ def action_supervision_loss(
     pre_ind, pre_label, eff_ind, eff_label = supervision
     pred_selected = torch.gather(pred, dim=1, index=pre_ind)
     pred_next_selected = torch.gather(pred_next, dim=1, index=eff_ind)
-    p1_loss = criterion_2(torch.cat([pred_selected, pred_next_selected], dim=1), torch.cat([pre_label, eff_label], dim=1))
+    preds = torch.cat([pred_selected, pred_next_selected], dim=1)
+    labels = torch.cat([pre_label, eff_label], dim=1)
+    p1_loss = criterion_2(preds, labels)
+    print(p1_loss)
+    print(preds)
+    print(labels)
     p2_loss = 0
     for p, p_n, u_ind in zip(pred, pred_next, eff_ind):
         diff = torch.tensor(list(n_pred.difference([i.item() for i in u_ind]))).to(device)
@@ -60,9 +65,6 @@ def action_supervision_loss(
         pred_next_unchange = torch.index_select(pred_next, dim=1, index=diff)
         p2_loss += criterion_3(pred_unchange, pred_next_unchange).sum(dim=1).mean()
     a_loss = criterion_2((pred_next - pred).detach(), change).sum(dim=1).mean()
-    print((pred_next - pred).detach())
-    print(p1_loss)
-
     return p1_loss.sum(dim=1).mean(), p2_loss, a_loss
 
 def has_grad(*tensor):
